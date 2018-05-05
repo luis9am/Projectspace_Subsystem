09c961c6849aa855327959852b14b474c4045b6c
Luis A Mireles <luui9mirel@gmail.com>
2018-05-05 14:09:26 -0600

    Added psm.py and workspace.py

---
+++ Project_Space_Manager.py
@@ -0,0 +1,24 @@
+import os
+from Project import *
+from Workspace import *
+
+
+def project_manager(action, workspace):
+    if (action == “getProjectName”)
+
+    | | action == “getProjectLayout” | | action == “getListOfProject”)
+    # check if workspace exists
+    try:
+        os.path.isfile(workspace)
+        get_workspace_name(workspace)
+    except OSError:
+        print('DNE')
+
+
+def main():
+    if:
+        get_
+
+
+if __name__ == "__main__":
+    main()
