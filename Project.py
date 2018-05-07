6b1209f282f035857187942ed71879408fb9da9e
Luis A Mireles <luui9mirel@gmail.com>
2018-05-06 22:28:38 -0600

    update project.py
    added xml func

--- Project.py
+++ Project.py
@@ -1,4 +1,6 @@
 import os
+import xml.etree.ElementTree as ET
+
 
 # Class Project collaborates with GUI and LUA script
 # Displays project name and its specific protocol.
@@ -45,76 +47,109 @@ class Project:
         self.description = value
 
 
-# creates new project directory with description
+# creates new project object and directory
 # @\requires (name != null or contain illegal characters) && (save_path exists)
 # @\ensures project exists at save_path with description text file
 def new_project(name, save_path, description):
     # Check if path exists
-    if verify_path(save_path):
-        # Set directory at save_path
+    try:
+        verify_path(save_path)
         os.chdir(save_path)
         try:
-            # If project name doesn't exists create new project
-            os.makedirs(name, 1) #
-            created_project = Project(name, save_path, "protocol", "layout")  # change to xml
-            c_project = created_project.get_name()
+            # If project dne create new project
+            os.makedirs(name, 1)  # Create directory at specified path
+            new_p = Project(name, save_path, "protocol", "layout", description)
 
-            # Create protocol file.xml
-            os.chdir(save_path + "/" + c_project)
-            filename1 = name + "_proto.xml"
-            proto_file = open(filename1, "w")
-            proto_file.write("Write Protocol here")
+            #
+            #   Load new project onto canvas(empty)
+            #
 
-            # Create text file that holds description (in SRS)
-            filename2 = name + "_description.txt"
-            desc_file = open(filename2, "w")
+            # Create text file that holds description
+            desc_file = open(name + "_description.txt", "w")
             desc_file.write(description)
         except OSError as e:
-            print('Directory name already exists: Directory not created.')
+            print('Directory already exists: Directory not created.')
+
+    except OSError as e:
+        print('Path not found')
 
 
-# Opens existing project onto gui
-# @\requires project != null
-# @\ensures saved project is open on gui
-def open_project(project):  # untested
+# Loads existing xml_project
+# @\requires xml_project != null
+# @\ensures saved xml_project is loaded
+def load_project(xml_project):  # untested needs gui
     try:
-        with open(project, 'r') as f:
-            contents = f.read()
-            # print(contents)
+        #   parses xml file
+        with open(xml_project, 'r') as f:
+            data = f.read()
+        tree = ET.fromstring(data)
+        # print 'Name:', tree.find('name').text
+        # print 'Path:', tree.find('path').text
+
+        #
+        #   Load xml Project onto canvas
+        #
+
     except IOError as e:
         print "Unable to open file"
 
 
-# save current project layout and protocol
+# save current project and save as XML
 # @\REQUIRES collaboration with layout & protocol to continue
 # @\ensures (new)project state is saved
 def save_project(project):  # untested
-    layout = "current layout"  # requires layout
-    protocol = "current proto"  # requires protocol
-    project.set_layout(layout)
-    project.set_protocol(protocol)
+    name = project.get_name()
+    path = project.get_path()
+    protocol = project.get_protocol()
+    layout = project.get_layout()
+    description = project.get_description()
 
 
 # export dissector in LUA script format
-# REQUIRES collab with lua script
+# REQUIRES collaboration with lua script
 # @\ensure file == lua format
-def export_lua_dissector(project, save_path):  # not working / take in project or xml file???
-    xml_file = project.get_path()
-    os.chdir(xml_file)
-    xml_file = xml_file + "\\" + project.get_name()
+def export_lua_dissector(xml_project, save_path):  # not working / take in project or xml file???
     try:
-        print "db:3"
-        filename = project.get_name() + ".lua"
-        fd = os.open(filename, os.O_CREAT | os.O_EXCL)
-        os.fdopen(fd, 'w')
+        #   Change path
+        verify_path(save_path)
+        os.chdir(save_path)
+        try:
+            #   Parse xml_file
+            with open(xml_project, 'r') as f:
+                data = f.read()
+            tree = ET.fromstring(data)
+            dissector_protocol = tree.find('protocol').text
+            dissector_name = tree.find('name').text
 
-    except OSError:
-        print('LUA dissector already exists: Dissector not created.')
+            #
+            #   Convert dissector_protocol to LUA format
+            #
+
+            lua_dissector = dissector_protocol
+
+            try:
+                #   Create dis.lua file in save_path
+                lua_dissector_name = dissector_name + ".lua"
+                fd = os.open(lua_dissector_name, os.O_CREAT | os.O_EXCL)
+                os.fdopen(fd, 'w')
+
+                #   write lua_dissector to LUA file
+                with open(lua_dissector_name, 'r') as f:        #   can't write to lua file??
+                    f.write(lua_dissector)
+
+            except OSError:
+                print('LUA dissector already exists: Dissector not created.')
+
+        except IOError as e:
+            print "Unable to open file"
+
+    except OSError as e:
+        print('Path not found')
 
 
-# adds self to workspace
-def add_to_workspace(project, workspace):  # not complete
-    print "not started"
+#   adds self to workspace
+# def add_to_workspace(project, workspace):  # not complete
+#     print "not started"
 
 
 # verify directory path
@@ -124,16 +159,20 @@ def verify_path(path):
 
 
 def main():
-    # testing
-    path1 = r"C:\Users\luui9\Desktop\test"  # annoying path requires char 'r' before string
-    dis = r"C:\Users\luui9\Desktop\test\new_project555"
-    # new_project("new_project555", path1, "description")
-    new_project = Project("test_project", path1, "protocool", "layout")  # works
+    print "\n"
+    #   testing
 
-    export_lua_dissector(new_project, path1)
+    path1 = r"C:\Users\luui9\Desktop\test"  # annoying path requires char 'r' before string
+    # dis = r"C:\Users\luui9\Desktop\test\new_project555"
+    # new_project("new_project555", path1, "description")
+    # new_project = Project("test_project", path1, "protocool", "layout")  # works
+
+    # export_lua_dissector(new_project, path1)
     # open_project("test.txt") # untested with projec
     # ts on gui
-
+    data = "C:\Users\luui9\PycharmProjects\Projectspace_Subsystem\data\pxml_file.xml"
+    # load_project(data)
+    export_lua_dissector(data, path1)
 
 if __name__ == "__main__":
     main()
